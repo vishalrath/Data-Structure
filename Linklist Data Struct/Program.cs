4e1b2371c5bdc317f225ebd3220b47d0633dc090
vishal <vishalrathod7038@gmail.com>
2021-10-27 23:36:32 +0530

    Linked list Assignment

---
+++ Linklist Data Struct/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Linklist_Data_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Welcome to the Linked list opration");
+            Uc1LinkedList list = new Uc1LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            Console.WriteLine("\n \nWelcome to the Linked list InsertFront opration");
+            Uc2LinkedList uc2list = new Uc2LinkedList();
+            uc2list.InsertFront(70);
+            uc2list.InsertFront(30);
+            uc2list.InsertFront(56);
+            list.Display();*/
+
+
+            /* Console.WriteLine("\n \nWelcome to the Linked list Apending  opration");
+             Uc3LinkedList uc3list = new Uc3LinkedList();
+             uc3list.InsertFront(70);
+            // uc3list.InsertFront(30);
+             uc3list.InsertFront(56);
+             uc3list.Display();
+            // uc3list.Append(56);
+             uc3list.Append(30);
+             //uc3list.Append(70);
+             uc3list.Display();*/
+
+
+            /* Console.WriteLine("\n \nInsert the middle node 56 and70 between 30  opration");
+             Uc4LinkedList uc4list = new Uc4LinkedList();
+             uc4list.InsertFront(56);
+             uc4list.InsertFront(70);
+             uc4list.MidInsert(30);
+             uc4list.Display();*/
+
+
+            /*Console.WriteLine("\n \nDelete first Node  opration");
+            Uc5LinkedList uc5list = new Uc5LinkedList();
+            uc5list.InserLast(56);
+            uc5list.InserLast(30);
+            uc5list.InserLast(70);
+            uc5list.Display();
+            Console.WriteLine(" \nDeleted first node valus is:" + uc5list.DeleteFirstNode());
+            uc5list.DeleteFirstNode();*/
+
+
+            /* Console.WriteLine("\n \nDelete Last Node  opration");
+             Uc6LinkedList uc6list = new Uc6LinkedList();
+             uc6list.InserLast(56);
+             uc6list.InserLast(30);
+             uc6list.InserLast(70);
+             uc6list.Display();
+              Console.WriteLine(" \nDeleted Last node valus is:" + uc6list.DeleteLastNode());
+              uc6list.Display();*/
+
+            /* Console.WriteLine("\n \nSearch  Node  opration");
+             Uc7LinkedList uc7list = new Uc7LinkedList();
+             uc7list.InserLast(56);
+             uc7list.InserLast(30);
+             uc7list.InserLast(70);
+             uc7list.Display();
+             Console.WriteLine(" \nSearch  node valus is:" + uc7list.search(30));
+             uc7list.Display();*/
+
+            /*  Console.WriteLine("\n \nAddAfter  Node  opration");
+                 Uc8LinkedList uc8add = new Uc8LinkedList();
+             uc8add.Add(56);
+             uc8add.Add(30);
+             uc8add.Add(70);
+             uc8add.Display();
+             uc8add.AddAfter(40);
+             uc8add.Display();*/
+
+           /* Console.WriteLine("\n \nSpecific delete  Node  opration");
+            Uc9LinkedList uc9list = new Uc9LinkedList();
+            uc9list.Add(56);
+            uc9list.Add(30);
+            uc9list.Add(70);
+            uc9list.AddAfter(40);
+            uc9list.Display();
+            uc9list.DeleteNode(40);
+            uc9list.Display();*/
+            
+
+            Console.WriteLine("\n \nSAscending Order of  Node  opration");
+            Uc10LinkedList uc10list = new Uc10LinkedList();
+            uc10list.Add(60);
+            uc10list.Add(56);
+            uc10list.Add(30);
+            uc10list.Add(70);
+            uc10list.Display();
+            uc10list.sort();
+            
+            uc10list.Display();
+           Console.ReadLine();
+
+        }
+    }
+}
---
+++ Linklist Data Struct/Uc10LinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linklist_Data_Struct
+{
+    class Uc10LinkedList
+    {
+        public Node head;   //defining the head possition of link list
+        public void Add(int deta)  // create add method in this class
+        {
+            Node node = new Node(deta);    // defining and creating object of node class and push the data of node class
+            if (this.head == null)
+            {
+                this.head = node;          // is null then push the node deta that have add into the head
+            }
+            else
+            {
+                Node temp = head;             // creating temp veriable of node
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("inserted into the link list:{0}", node.data);
+        }
+        public void Display()  //display the data in linked list
+        {
+            Console.WriteLine("Display Nodes");
+            Node temp = this.head;               // initializethe temp veriable in the node class
+            if (temp == null)                  // checking linklist empty or not
+            {
+                Console.Write("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + "");
+                    temp = temp.next;
+                }
+            }
+        }
+
+        /* This is the main function that sorts 
+   the linked list.*/
+       /* Node newNode(int key)
+        { 
+            return new Node(key);
+        }*/
+       public  void sort()
+        {
+           
+
+            /* Create 2 dummy nodes and initialise as 
+            heads of linked lists */
+            Node Ahead = new Node(0), Dhead = new Node(0);
+
+           
+            // Split the list into lists
+            splitList(Ahead, Dhead);
+
+            Ahead = Ahead.next;
+            Dhead = Dhead.next;
+
+            // reverse the descending list
+            Dhead = reverseList(Dhead);
+
+            // merge the 2 linked lists
+            head = mergeList(Ahead, Dhead);
+        }
+
+        /* Function to reverse the linked list */
+       Node reverseList(Node Dhead)
+        {
+            Node current = Dhead;
+            Node prev = null;
+            Node next;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            Dhead = prev;
+            return Dhead;
+        }
+
+        /* Function to print linked list */
+      /*public void printList()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }*/
+
+        // A utility function to merge two sorted linked lists
+        Node mergeList(Node head1, Node head2)
+        {
+            // Base cases
+            if (head1 == null)
+                return head2;
+            if (head2 == null)
+                return head1;
+
+            Node temp = null;
+            if (head1.data < head2.data)
+            {
+                temp = head1;
+                head1.next = mergeList(head1.next, head2);
+            }
+            else
+            {
+                temp = head2;
+                head2.next = mergeList(head1, head2.next);
+            }
+            return temp;
+        }
+
+        // This function alternatively splits a linked list with head
+        void splitList(Node Ahead, Node Dhead)
+        {
+            Node ascending = Ahead;        // "Ahead" is reference to head of ascending linked list
+            Node dscn = Dhead;        // "Dhead" is reference to head of descending linked list
+            Node curr = head;
+
+            // Link alternate nodes
+
+            while (curr != null)
+            {
+                // Link alternate nodes in ascending order
+                ascending.next = curr;
+                ascending = ascending.next;
+                curr = curr.next;
+
+                if (curr != null)
+                {
+                    dscn.next = curr;
+                    dscn = dscn.next;
+                    curr = curr.next;
+                }
+            }
+
+            ascending.next = null;
+            dscn.next = null;
+        }
+
+    }
+}
